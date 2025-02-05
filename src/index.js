a68a14575ad892168f3a80fc3fd6be09f95e966e
akshay <66055053+akshay-A09@users.noreply.github.com>
2025-02-05 12:19:25 +0530

    text change

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     {/* /testing/agency09/ */}
+     {/* /agency09/ */}
      <Router basename={'/'}>
      <ScrollToTop />
       <Routes>
